ff5ebb1eb4dd5f79534e36a562f90cd8080b1354
l1tch <38979682+l1tch@users.noreply.github.com>
2023-04-04 16:27:20 +0200

    dumb fix

--- code/gohome.py
+++ code/gohome.py
@@ -1,18 +1,15 @@
 def countOrd(Uarr):
     leng = Uarr[0]+1                                                                     
-    Oarr = [None] * (leng)                  
+    Oarr = [0] * (leng)                  
     
     for i in Uarr:                          
         if leng < i+1:                      
             nLen = (i+1) - leng
             leng = i+1
-            Oarr = [*Oarr, * [None] * nLen]
-        if Oarr[i] is None:
-            Oarr[i] = 1
-        else:
-            Oarr[i] += 1      
+            Oarr = [*Oarr, * [0] * nLen]
+        Oarr[i] += 1      
     lst=[]
     for indx, val in enumerate(Oarr):
-        if val != None:
+        if val:
             lst+=[indx] * val
     return lst
